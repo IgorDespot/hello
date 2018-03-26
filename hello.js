4af3733b0b995c1e27397caa99d9b3233e247a35
Igor Despot <igordesspot@gmail.com>
2018-03-26 16:48:49 +0200

    sasa

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
+}).listen(3000, '192.168.240.127');
+console.log('Server running at http://192.168.240.127:3000/');
